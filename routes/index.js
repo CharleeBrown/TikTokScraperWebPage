1dd237149e3df4db3afa64915b75d412b6f0bfef
CharlesLee-23 <Coleman.hallom@Spray.com>
2022-03-02 14:47:43 -0600

    added

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var tiktok = require('tiktok-scraper');
+var tito = require('tiktok-scraper');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
- 
+ tito.user('', function(err,next));
 });
 
 module.exports = router;
