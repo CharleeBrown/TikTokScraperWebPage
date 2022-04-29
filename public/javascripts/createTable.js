15731fcb082ace127af519d0eef768fb3a82e5e7
CharlesLee-23 <Coleman.hallom@Spray.com>
2022-04-29 15:36:35 -0500

    Added comments

--- public/javascripts/createTable.js
+++ public/javascripts/createTable.js
@@ -2,53 +2,58 @@
 // The number of cells will be four for testing.
 
 
-
+//Table itself
 var tableMain = document.createElement('table');
 
+//Header
 var tableHead = document.createElement('thead');
+
+// Header Row
 var headRow = document.createElement('tr');
+
+//Body variable
 var docBody = document.getElementsByTagName('body');
+
+//Header Cells
 var headCell = document.createElement('th');
-headCell.style.width = "30px";
-headCell.style.height = "30px";
-
-
-
 
 function tableMade(numbers){
 	let tableRow = document.createElement('tr');
-
+	
+	//Table Styling
+	headCell.style.width = "30px";
+	headCell.style.height = "30px";
 	tableMain.style.border = "1px solid black";
 	tableMain.style.borderCollapse = 'collapse';
 	tableMain.style.margin= "0 auto";
 	headCell.style.border="1px solid black";
 	headCell.colSpan="5";
-	headRow.appendChild(headCell);
-	headRow.appendChild(headCell);
-
 	tableHead.style.height = "30px";
+
+	headRow.appendChild(headCell);
+	headRow.appendChild(headCell);
+
 	tableHead.appendChild(headRow);
+	tableMain.appendChild(tableHead);
 
 
-	tableMain.appendChild(tableHead)
 	for(var i = 0; i < numbers; i++){
 		let tableRow1 = document.createElement('tr');
-		var tableCell = document.createElement('td');
+		let tableCell = document.createElement('td');
 		
 		tableCell.style.border = "1px solid black";
 		tableCell.style.width = "80px";
 		tableCell.style.height= "80px";
 		tableRow.appendChild(tableCell);
+		
+		//If the index is 5, append the row to the table, create a new row.
 		if(i%5==0 && i !=0){
-			console.log(i%4);
-			//tableMain.appendChild(tableRow);
+			
 			tableRow1 = tableRow;
 			tableMain.appendChild(tableRow1);
 			
 		}
 	}
-	
-		console.log("test");
 	docBody[0].appendChild(tableMain);
 	
 }
