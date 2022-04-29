44a7664e72f985c3b8766b5035ab48042251a18b
CharlesLee-23 <Coleman.hallom@Spray.com>
2022-04-29 13:20:46 -0500

    Centered. Font Added. Working on table script.

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ let tito = require('tiktok-scraper');
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Placeholder' });
 
 });
 
