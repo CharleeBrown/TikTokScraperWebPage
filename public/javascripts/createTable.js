a4f8e9eeddb40f088714c0c6374ba574904b231d
CharlesLee-23 <Coleman.hallom@Spray.com>
2022-04-29 15:30:43 -0500

    Fixed header. Body still bad.

--- public/javascripts/createTable.js
+++ public/javascripts/createTable.js
@@ -5,18 +5,32 @@
 
 var tableMain = document.createElement('table');
 
-var tableHead = document.createElement('th');
+var tableHead = document.createElement('thead');
+var headRow = document.createElement('tr');
 var docBody = document.getElementsByTagName('body');
-
+var headCell = document.createElement('th');
+headCell.style.width = "30px";
+headCell.style.height = "30px";
 
 
 
 
 function tableMade(numbers){
 	let tableRow = document.createElement('tr');
+
 	tableMain.style.border = "1px solid black";
 	tableMain.style.borderCollapse = 'collapse';
-	
+	tableMain.style.margin= "0 auto";
+	headCell.style.border="1px solid black";
+	headCell.colSpan="5";
+	headRow.appendChild(headCell);
+	headRow.appendChild(headCell);
+
+	tableHead.style.height = "30px";
+	tableHead.appendChild(headRow);
+
+
+	tableMain.appendChild(tableHead)
 	for(var i = 0; i < numbers; i++){
 		let tableRow1 = document.createElement('tr');
 		var tableCell = document.createElement('td');
@@ -26,9 +40,9 @@ function tableMade(numbers){
 		tableCell.style.height= "80px";
 		tableRow.appendChild(tableCell);
 		if(i%5==0 && i !=0){
-			
+			console.log(i%4);
 			//tableMain.appendChild(tableRow);
-			
+			tableRow1 = tableRow;
 			tableMain.appendChild(tableRow1);
 			
 		}
