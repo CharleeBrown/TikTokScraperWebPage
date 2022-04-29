ef8d6ff11b02552c00b33626b8026c1452499bcd
CharlesLee-23 <Coleman.hallom@Spray.com>
2022-04-29 15:23:37 -0500

    GOAL: every 5th cell = new row. CURRENT: one row

--- public/javascripts/createTable.js
+++ public/javascripts/createTable.js
@@ -1,31 +1,45 @@
 // SUMMARY: This script will generate a table for query results.
 // The number of cells will be four for testing.
 
-const cells = 4;
-var tableCell = document.createElement('td');
+
+
 var tableMain = document.createElement('table');
-var tableRow = document.createElement('tr');
+
 var tableHead = document.createElement('th');
 var docBody = document.getElementsByTagName('body');
 
 
 
-tableMain.style.border = "1px solid black";
-tableMain.style.borderCollapse = 'collapse';
+
 
 function tableMade(numbers){
+	let tableRow = document.createElement('tr');
+	tableMain.style.border = "1px solid black";
+	tableMain.style.borderCollapse = 'collapse';
+	
 	for(var i = 0; i < numbers; i++){
+		let tableRow1 = document.createElement('tr');
+		var tableCell = document.createElement('td');
 		
+		tableCell.style.border = "1px solid black";
+		tableCell.style.width = "80px";
+		tableCell.style.height= "80px";
 		tableRow.appendChild(tableCell);
-		tableMain.append(tableRow);
-		console.log("test");
+		if(i%5==0 && i !=0){
+			
+			//tableMain.appendChild(tableRow);
+			
+			tableMain.appendChild(tableRow1);
+			
+		}
 	}
 	
+		console.log("test");
 	docBody[0].appendChild(tableMain);
 	
 }
 
-tableMade(10);
+
 
 
 
